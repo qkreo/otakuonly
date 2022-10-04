d86de1833873858ee6de983a9b9eed213cb03358
DESKTOP-TGHAAU4\박ㄷ현 <bdh407@naver.com>
2022-10-05 00:35:39 +0900

    최종수정본 v_1.1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ connect(); // 스키마연결 함수 실행
 app.use(express.json()); // 모든코드에서 body parser 를 등록해서 전역에 사용한다
 
 app.get('/', (req, res) => {
-  res.send("대문 / 입장하기?");    
+  res.json({"msg":"접속을 환영합니다","/index":"전체게시글 보기","index/post/:postId":"상세게시글 보기","index/post/:postId/comments":"전체 댓글보기","comments/comments_id":"상세게시글"});    
 });
 
 app.use("/index",[indexRouter]); // 인덱스 라우터 사용
